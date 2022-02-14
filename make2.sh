a3a17b1e5b71cacc3a2898f548934007e0413333
carlosalberto.magana.lopez <carlosalberto.magana.lopez@bbva.com>
2022-02-14 12:59:10 -0600

    Commit-inicial

---
+++ make2.sh
@@ -0,0 +1,6 @@
+﻿	touch README.txt
+	echo "El proyecto es: Tarea_4oModulo" > README.txt
+	echo "información de MikeFile: " >> README.txt
+	ls -al adivinanza.sh >> README.txt
+	echo "el programa contiene el siguiente numero de lineas:" >> README.txt
+	wc -l adivinanza.sh >> README.txt
