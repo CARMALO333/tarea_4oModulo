cb52564c501a2849649654220a61609d757518f1
carlosalberto.magana.lopez <carlosalberto.magana.lopez@bbva.com>
2022-02-15 08:37:28 -0600

    Se modifica MAke

--- make2.sh
+++ make2.sh
@@ -1,6 +1,10 @@
 ﻿	touch README.md
 	echo "El proyecto es: Tarea_4oModulo" > README.md
+	echo "
+	" >> README.md
 	echo "información de MikeFile: " >> README.md
 	ls -al adivinanza.sh >> README.md
+	echo "
+	" >> README.md
 	echo "el programa contiene el siguiente numero de lineas:" >> README.md
 	wc -l adivinanza.sh >> README.md
